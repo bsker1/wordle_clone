e2c5f9f107360cf063832f734d9daeced5b87a67
Brandon Kiser <kiserb17@gmail.com>
2022-11-15 02:39:47 -0500

    initializing guessed words array, displaying grid outline for game with guessed word array contents

--- main.cpp
+++ main.cpp
@@ -1,10 +1,67 @@
 #include <iostream>
+#include <fstream>
+#include <ctime>
+#include <stdlib.h>
 
-using namespace std;
+const int wordCount = 21952;
+
+//  Picks random word from dictionary to become the desired answer
+std::string pickAnswer()
+{
+    std::ifstream dictionary;
+    dictionary.open("wordledictionary.txt");
+
+    std::string answer;
+    srand(time(NULL));
+    int index = rand() % wordCount;
+    for (int i = 0; i <= index; ++i)
+    {
+        dictionary >> answer;
+    }
+
+    dictionary.close();
+    return answer;
+}
+
+//  Initializes passed string
+void initString(std::string& inString)
+{
+    inString = "     ";
+}
+
+//  Generates grid where each guessed word is displayed
+void generateGrid(std::string guessedWords[])
+{
+    for (int i = 0; i < 6; ++i)
+    {
+        for (int j = 0; j < 5; ++j)
+        {
+            std::cout << "| " << guessedWords[i][j] << " ";
+        }
+        std::cout << "|\n";
+    }
+}
 
 int main()
 {
-    
+    std::string answer = pickAnswer();
+
+    std::string guessedWords[6];
+    for (int i = 0; i < 6; ++i)
+    {
+        initString(guessedWords[i]);
+    }
+
+    generateGrid(guessedWords);
 
     return 0;
 }
+
+/*
+    | . | . | . | . | . |
+    | . | . | . | . | . |
+    | . | . | . | . | . |
+    | . | . | . | . | . |
+    | . | . | . | . | . |
+    | . | . | . | . | . |
+*/

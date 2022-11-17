518627604f35483da978492099184982b398a262
Brandon Kiser <kiserb17@gmail.com>
2022-11-17 15:41:46 -0500

    added error handling to word guess input, hints are generated and displayed to the user after each guess

--- source/functions.cpp
+++ source/functions.cpp
@@ -7,6 +7,11 @@ std::string generateAnswer()
 {
     std::ifstream dictionary;
     dictionary.open("wordledictionary.txt");
+    if (!dictionary)
+    {
+        std::cerr << "fstream error" << std::endl;
+        std::cin.get();
+    }
 
     srand(time(NULL));
     std::string answer;
@@ -20,7 +25,7 @@ std::string generateAnswer()
     return answer;
 }
 
-//  Prompts the user to enable hard mode or not
+//  Prompts the user to enable hard mode or not, returns true is enabled
 bool chooseMode()
 {
     std::cout << "Enable hard mode?" << std::endl;
@@ -90,9 +95,9 @@ void initVector(std::vector<std::string> &inVect)
 //  Main game loop
 void guessLoop(std::vector<std::string> &guessedWords)
 {
-    bool mode = chooseMode();
+    //bool hardMode = chooseMode();
 
-    std::string answer = "lease";
+    std::string answer = generateAnswer();
     std::cout << "TESTING: answer is " << answer << std::endl;
     std::vector<std::string> hints;
     initVector(hints);
@@ -102,9 +107,10 @@ void guessLoop(std::vector<std::string> &guessedWords)
     {
         generateGrid(guessedWords, hints);
 
-        std::cout << "Guess a word: ";
-        clearBuffer();
-        std::cin >> guessedWords[turn];
+        getGuess(guessedWords[turn]);
+
+        generateHints(answer, guessedWords[turn], hints[turn]);
+
         if (++turn >= 6)
         {
             loopFlag = false;
@@ -130,5 +136,124 @@ void generateGrid(std::vector<std::string> guessedWords,
             std::cout << "   " << hints[i][j];
         }
         std::cout << std::endl;
+    }
+}
+
+void getGuess(std::string &inString)
+{
+    bool loopFlag = true;
+    while (loopFlag)
+    {
+        std::cout << "Guess a word" << std::endl;
+        std::cout << "\n*__ ";
+        std::cin >> inString;
+        if (validateGuess(inString))
+        {
+            loopFlag = false;
+        }
+        else
+        {
+            clearBuffer();
+        }
+    }
+}
+
+//  Returns false if input fails any format check, true otherwise
+bool validateGuess(std::string &inString)
+{
+    if (std::cin.fail())
+    {
+        throwInputError();
+        return false;
+    }
+
+    if (inString.size() != 5)
+    {
+        std::cerr << "ERROR: word must be 5 letters long" << std::endl;
+        return false;
+    }
+
+    for (int i = 0; i < 5; ++i)
+    {
+        inString[i] = tolower(inString[i]);
+        if (inString[i] < 97 || inString[i] > 122)
+        {
+            std::cerr << "ERROR: word must only contain letters" << std::endl;
+            return false;
+        }
+    }
+
+    if (!inDictionary(inString))
+    {
+        std::cerr << "ERROR: word does not exist" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool inDictionary(std::string inString)
+{
+    std::ifstream dictionary;
+    dictionary.open("wordledictionary.txt");
+    if (!dictionary)
+    {
+        std::cerr << "fstream error" << std::endl;
+        std::cin.get();
+    }
+    std::string currentWord;
+
+    while (dictionary >> currentWord)
+    {
+        if (inString == currentWord)
+        {
+            dictionary.close();
+            return true;
+        }
+    }
+
+    dictionary.close();
+    return false;
+}
+
+void generateHints(std::string answer, std::string guess, std::string &hint)
+{
+    hint = "xxxxx";
+    checkGreen(answer, guess, hint);
+    checkYellow(answer, guess, hint);
+}
+
+void checkGreen(std::string answer, std::string guess, std::string &hint)
+{
+    for (int i = 0; i < 5; ++i)
+    {
+        if (guess[i] == answer[i])
+        {
+            hint[i] = 'G';
+        }
+    }
+}
+
+void checkYellow(std::string answer, std::string guess, std::string &hint)
+{
+    for (int i = 0; i < 5; ++i)
+    {
+        //  Don't consider the character if it's already been marked green
+        if (hint[i] == 'G')
+        {
+            continue;
+        }
+        for (int j = 0; j < 5; ++j)
+        {
+            //  Don't consider the character if it's already been marked as a hint
+            if (hint[j] != 'x')
+            {
+                continue;
+            }
+            if (guess[i] == answer[j])
+            {
+                hint[i] = 'Y';
+            }
+        }
     }
 }
